68077ead8b180ee5f282d815ef4cdc36e5041f85
timcpau <timchpau@gmail.com>
2021-12-30 01:15:01 +0300

    Updating code to work with just console and alerts

---
+++ script.js
@@ -0,0 +1,134 @@
+
+let playerSelection;
+let computerSelection;
+let computerScore = 0;
+let playerScore = 0;
+let roundResult;
+
+function computerPlay() {
+    let choice = ["rock", "scissors", "paper"];
+
+    return choice[Math.floor(Math.random() * choice.length)];
+
+}
+
+console.log(computerPlay())
+
+function noOneWonRocks() {
+    alert("No one won, rocks are equal");
+    return alert(`Your score: ${playerScore}, computer's score: ${computerScore}`);
+}
+
+function noOneWonScissors() {
+    alert("No one won, scissors are equal");
+    return alert(`Your score: ${playerScore}, computer's score: ${computerScore}`);
+}
+
+function noOneWonPaper() {
+    alert("No one won, papers are equal");
+    return alert(`Your score: ${playerScore}, computer's score: ${computerScore}`);
+}
+
+function playerWonRock() {
+    playerScore = ++playerScore;
+    alert("You won, rock beats scissors!");
+    return alert(`Your score: ${playerScore}, computer's score: ${computerScore}`);
+}
+
+function computerWonPaper() {
+    computerScore = ++computerScore;
+    alert("You lose, paper beats rock!");
+    return alert(`Your score: ${playerScore}, computer's score: ${computerScore}`);
+}
+
+function computerWonRock() {
+    computerScore = ++computerScore;
+    alert("You lose, rock beats scissors");
+    return alert(`Your score: ${playerScore}, computer's score: ${computerScore}`);
+}
+
+function playerWonScissors() {
+    playerScore = ++playerScore;
+    alert("You won, scissors beats paper!");
+    return alert(`Your score: ${playerScore}, computer's score: ${computerScore}`);
+}
+
+function playerWonPaper() {
+    playerScore = ++playerScore;
+    alert("You won, paper beats rock!");
+    return alert(`Your score: ${playerScore}, computer's score: ${computerScore}`);
+}
+
+function computerWonScissors() {
+    computerScore = ++computerScore;
+    alert("You lose, scissors beats paper");
+    return alert(`Your score: ${playerScore}, computer's score: ${computerScore}`);
+}
+
+function playRound(playerSelection, computerSelection) {
+
+    playerSelection = prompt('Chose one of those options: "rock", "paper", "scissors": ', '');
+    computerSelection = computerPlay();
+
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerPlay();
+    
+    if (playerSelection === "rock" & computerSelection === "rock") {
+        roundResult = noOneWonRocks();
+        return roundResult;
+    } else if (playerSelection === "scissors" & computerSelection === "scissors") {
+        roundResult = noOneWonScissors();
+        return roundResult;
+    } else if (playerSelection === "paper" & computerSelection === "paper") {
+        roundResult = noOneWonPaper();
+        return roundResult;
+    } else if (playerSelection === "rock" & computerSelection === "scissors") {
+        roundResult = playerWonRock();
+        console.log(playerScore);
+        return roundResult;
+    } else if (playerSelection === "rock" & computerSelection === "paper") {
+        roundResult = computerWonPaper();
+        console.log(computerScore);
+        return roundResult;
+    } else if (playerSelection === "scissors" & computerSelection === "rock") {
+        roundResult = computerWonRock();
+        console.log(computerScore);
+        return roundResult;
+    } else if (playerSelection === "scissors" & computerSelection === "paper") {
+        roundResult = playerWonScissors();
+        console.log(playerScore);
+        return roundResult;
+    } else if (playerSelection === "paper" & computerSelection === "rock") {
+        roundResult = playerWonPaper();
+        console.log(playerScore);
+        return roundResult;
+    } else if (playerSelection === "paper" & computerSelection === "scissors") {
+        roundResult = computerWonScissors();
+        console.log(computerScore);
+        return roundResult;
+    } else {
+        alert("Oh, you should've insert one of those: rock, scissors, paper.");   
+    }    
+}
+
+for (let i = 0; i < 1000; i++) {
+    let roundResult = playRound(playerSelection, computerSelection);
+    console.log("Your score is: " + playerScore + ". Computer's score is: " + computerScore);
+    if (playerScore === 5 || computerScore === 5) {
+        break;
+    }
+}
+
+function declareRoundResult() {
+    if (computerScore === 5){
+        alert("Computer won!");
+    } else if (playerScore === 5) {
+        alert("You won. congradulations!");
+    }
+}
+
+console.log(playRound(playerSelection, computerSelection))
+
+declareRoundResult();
+
+console.log(declareRoundResult())
